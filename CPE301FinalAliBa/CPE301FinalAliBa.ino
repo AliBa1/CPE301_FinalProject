c7df50e02f3aec5eda18ad184d95048c065bd3ea
Ali Ba <aliba@nevada.unr.edu>
2023-11-15 17:49:15 -0800

    Added code for Fan and Vent motors

--- CPE301FinalAliBa/CPE301FinalAliBa.ino
+++ CPE301FinalAliBa/CPE301FinalAliBa.ino
@@ -1,28 +1,54 @@
 /*
 Breadboard Setup
-PK2 (Analog In A10) = Button
-PD0 (Communication 21) = Yellow LED
-PD1 (Communication 20) = Green LED
-PD2 (Communication 19) = Blue LED
-PD3 (Communication 18) = Red LED
+Start Button = PK2 (Analog In A10)
+Vent (Stepper Motor) Button = PK3 (Analog In A11)
+Yellow LED = PD0 (Communication 21)
+Green LED = PD1 (Communication 20)
+Blue LED = PD2 (Communication 19)
+Red LED = PD3 (Communication 18)
 LCD RS (PWM 11) = 11
 LCD EN (PWM 12) = 12
 LCD D4 (PWM 2) = 2
 LCD D5 (PWM 3) = 3
 LCD D6 (PWM 4) = 4
 LCD D7 (PWM 5) = 5
+Stepper Motor IN1 = 7
+Stepper Motor IN2 = 8
+Stepper Motor IN3 = 9
+Stepper Motor IN4 = 10
+DC (Fan) Motor Speed Pin: 1 = PD4 (Communication 17)
+DC (Fan) Motor IN1: 2 = PD5 (Communication 16)
+DC (Fan) Motor IN2: 7 = PD6 (Communication 15)
+*/
+
+/*
+To Test:
+LEDs
+Stepper Motor
+Fan
+Buttons
+Serial Port
+Clock
+LCD
 */
 
 // Download time library 1.6.1
 
 #include <LiquidCrystal.h>
 #include <time.h>
+#include <Stepper.h>
 
 #define WRITE_HIGH_PD(pin_num)  *port_d |= (0x01 << pin_num);
 #define WRITE_LOW_PD(pin_num)  *port_d &= ~(0x01 << pin_num);
 
 #define RDA 0x80
 #define TBE 0x20
+
+int state;
+// 0 = DISABLED
+// 1 = IDLE
+// 2 = RUNNING
+// 3 = ERROR
 
 // Define Port K Register Pointers
 volatile unsigned char* port_k = (unsigned char*) 0x108; 
@@ -41,11 +67,19 @@ volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
 volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
 volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
 
-int state;
-// 0 = DISABLED
-// 1 = IDLE
-// 2 = RUNNING
-// 3 = ERROR
+// Defines the number of steps per rotation
+const int stepsPerRevolution = 2038;
+
+// Creates an instance of stepper class
+// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
+Stepper myStepper = Stepper(stepsPerRevolution, 7, 9, 8, 10);
+
+//DC (Fan) Motor Pins
+int speedPin = 4;
+int dir1 = 5;
+int dir2 = 6;
+//DC (Fan) Motor Speed
+int mSpeed = 90;
 
 const int RS = 11, EN = 12, D4 = 2, D5 = 3, D6 = 4, D7 = 5;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
@@ -60,9 +94,27 @@ void setup()
   
   //set PD0 to OUTPUT
   *ddr_d |= 0x01;
+  //set PD1 to OUTPUT
+  *ddr_d |= 0x02;
+  //set PD2 to OUTPUT
+  *ddr_d |= 0x04;
+  //set PD3 to OUTPUT
+  *ddr_d |= 0x08;
+  
+  
+  //set PD4 to OUTPUT
+  *ddr_d |= 0x10;
+  //set PD5 to OUTPUT
+  *ddr_d |= 0x20;
+  //set PD6 to OUTPUT
+  *ddr_d |= 0x40;
 
   //set PK2 to INPUT
   *ddr_k &= 0xFB;
+
+  
+  //set PK3 to INPUT
+  *ddr_k &= 0xF7;
   
   // enable the pullup resistor on PK2
   *port_k |= 0x04;
@@ -83,6 +135,19 @@ void loop()
     *pin_k = *pin_k & 0xFB;
   }
 
+  bool ventButton;
+  if(*pin_k & 0x08) {
+    ventButton = true;
+    // To keep true without holding button. Test to make sure it works
+    // PD3
+    *pin_k = *pin_k & 0x08;
+  } else {
+    ventButton = false;
+    // To keep false without holding button. Test to make sure it works
+    // PD3
+    *pin_k = *pin_k & 0xF7;
+  }
+
   switch (state)
   {
     case 0: // Time of state change and motor position change to serial port
@@ -91,11 +156,16 @@ void loop()
 
               U0putchar('\n');
             // Fan OFF
+              stopFan();
             // Yellow LED ON
               // drive PD0 HIGH
               WRITE_HIGH_PD(0);
-            // Stop fan motor
+            // Stop stepper motor
+              stopStepperMotor();
             // Monitor start button
+              if (startButton) {
+                state = 1;
+              }
             break;
     case 1: // Time of state change and motor position change to serial port
               //timeToSerial();
@@ -103,17 +173,23 @@ void loop()
               
               U0putchar('\n');
             // Fan OFF
+              stopFan();
             // Green LED ON
               // drive PD1 HIGH
               WRITE_HIGH_PD(1);
             // LCDTempAndHumidity() ON
-            // Stop fan motor
             // Monitor water level
             // Respond to change in vent control
+              if (ventButton) {
+                startStepperMotor();
+              } else {
+                stopStepperMotor();
+              }
             // if (temp>threshhold) {state=2}
             // if (waterLevel<=threshold) {state=3}
             // if (stopButtonPressed) {state=0}
               if (!startButton) {
+                stopFan();
                 state = 0;
               }
             break;
@@ -124,6 +200,7 @@ void loop()
               U0putchar('\n');
             // LCDTempAndHumidity() ON
             // Start fan motor
+              startFan();
             // Blue LED ON
               // drive PD2 HIGH
               WRITE_HIGH_PD(2);
@@ -133,10 +210,16 @@ void loop()
               WRITE_LOW_PD(3);
             // Monitor water level
             // Respond to change in vent control
+              if (ventButton) {
+                startStepperMotor();
+              } else {
+                stopStepperMotor();
+              }
             // if (temp<=threshhold) {state=1}
             // if (waterLevel<threshold) {state=3}
             // if (stopButtonPressed) {state=0}
               if (!startButton) {
+                stopFan();
                 state = 0;
               }
             break;
@@ -146,6 +229,8 @@ void loop()
               
               U0putchar('\n');
             // Motor OFF
+              stopStepperMotor();
+              stopFan();
             // Write error to LCD
               LCDError();
             // Red LED ON (All other leds off)
@@ -156,9 +241,15 @@ void loop()
               WRITE_LOW_PD(1);
               WRITE_LOW_PD(2);
             // Respond to change in vent control
+              if (ventButton) {
+                startStepperMotor();
+              } else {
+                stopStepperMotor();
+              }
             // if(resetPressed) {state=1}
             // if (stopButtonPressed) {state=0}
               if (!startButton) {
+                stopFan();
                 state = 0;
               }
             break;
@@ -278,6 +369,54 @@ void stateToSerial(int state){
   U0putchar('R ');
  }
 }
+
+void startStepperMotor() {
+  // Rotate CW slowly at 5 RPM
+  myStepper.setSpeed(5);
+  myStepper.step(stepsPerRevolution);
+  delay(1000);
+
+  // Print vent position change to serial port
+  U0putchar('S');
+  U0putchar('T');
+  U0putchar('E');
+  U0putchar('P');
+  U0putchar('P');
+  U0putchar('E');
+  U0putchar('R ');
+  U0putchar('P');
+  U0putchar('O');
+  U0putchar('S ');
+  U0putchar('C');
+  U0putchar('H');
+  U0putchar('A');
+  U0putchar('N');
+  U0putchar('G');
+  U0putchar('E');
+  U0putchar('D');
+}
+
+void stopStepperMotor() {
+  // Rotate CW at 0 RPM
+  myStepper.setSpeed(0);
+  myStepper.step(stepsPerRevolution);
+  delay(1000);
+}
+
+
+void startFan() {
+  WRITE_LOW_PD(dir1);
+  WRITE_HIGH_PD(dir2);
+  WRITE_HIGH_PD(speedPin);
+  delay(25);
+}
+
+void stopFan() {
+  WRITE_LOW_PD(dir1);
+  WRITE_LOW_PD(dir2);
+  WRITE_LOW_PD(speedPin);
+}
+
 // UART Stuff
 void U0init(int U0baud)
 {
